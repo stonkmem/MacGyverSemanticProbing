38383190bc08cbd599858f7857981fe7598ef355
stonkmem <stonkeroctober@gmail.com>
2024-09-25 17:45:52 +0800

    Installed Llama 8B Instruct, with simple prompt (:

---
+++ sample_query_Llama.py
@@ -0,0 +1,11 @@
+from transformers import pipeline
+from loginscript import hf_login
+
+hf_login()
+
+
+messages = [
+    {"role": "user", "content": "Who are you?"},
+]
+pipe = pipeline("text-generation", model="meta-llama/Meta-Llama-3.1-8B-Instruct")
+pipe(messages)
