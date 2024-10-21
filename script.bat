1af839cb20420b8e8a7685db3ead6661e507e129
apxy <107977710+apxy36@users.noreply.github.com>
2024-10-21 10:09:30 +0800

    Update script.bat

--- script.bat
+++ script.bat
@@ -2,7 +2,7 @@ nvidia-smi && nvcc --version
 set CMAKE_ARGS="-DGGML-CUDA=ON"
 set FORCE_CMAKE=1
 pip install llama-cpp-python --force-reinstall --upgrade --no-cache-dir --verbose
-pip install numpy tenacity pandas matplotlib seaborn huggingface-hub tranformers openai
+pip install numpy tenacity pandas matplotlib seaborn huggingface-hub tranformers openai datasets torch 
 echo $CUDA_HOME
 
 @REM pip install llama-cpp-python
