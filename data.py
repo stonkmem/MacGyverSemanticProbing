f1a7616aa1ce97b05e9ce4e95e1f33fafd50f1df
apxy <107977710+apxy36@users.noreply.github.com>
2024-10-21 10:34:14 +0800

    Update data.py

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+# import matplotlib.pyplot as plt
+# import seaborn as sns
 
 df = pd.read_excel('https://github.com/allenai/MacGyver/raw/refs/heads/main/data/MacGyver/problem_solution_pair.xlsx')
 df_extra = pd.read_excel('https://github.com/allenai/MacGyver/raw/refs/heads/main/data/MacGyver/additional_human_solutions.xlsx')
