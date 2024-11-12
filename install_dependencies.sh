ec0e3ad1170863774f1813481bcb5f33c1fc4687
apxy <ckc.zachary@yahoo.com>
2024-11-12 17:23:21 +0800

    edits

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -9,11 +9,11 @@
 # scp .env <aspire_user>@jumphost.ntu.edu.sg:/path/on/jumphost/.env /home/<your_username>/macgyversemanticprobing/.env
 
 # Set environment variables
-export CMAKE_ARGS="-DGGML-CUDA=ON"
-export FORCE_CMAKE=1
+# export CMAKE_ARGS="-DGGML-CUDA=ON"
+# export FORCE_CMAKE=1
 
 # Install llama-cpp-python
-pip install llama-cpp-python --force-reinstall --upgrade --no-cache-dir --verbose
+# pip install llama-cpp-python --force-reinstall --upgrade --no-cache-dir --verbose
 
 # Install additional Python packages
 pip install numpy tenacity pandas matplotlib seaborn huggingface-hub transformers openai datasets torch accelerate>=0.26.0 
