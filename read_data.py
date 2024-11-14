51bbe8d149a5d142be66c8f1ace64df025d69d99
apxy36 <107977710+apxy36@users.noreply.github.com>
2024-11-14 10:45:03 +0800

    Update read_data.py

--- read_data.py
+++ read_data.py
@@ -1,10 +1,19 @@
 import json
+import sys
 
 def read_txt_file_as_json(txt_file_path):
     with open(txt_file_path, 'r') as txt_file:
         data = json.load(txt_file)
     return data
 
+def read_json_file(json_file_path):
+    with open(json_file_path, 'r') as json_file:
+        data = json.load(json_file)
+    return data
+
+file_name = "results.json"
+if len(sys.argv) > 1:
+    file_name = sys.argv[1]
 # Example use case
-file_contents = read_txt_file_as_json("outputs.txt")
-print(file_contents)
+data = read_json_file(file_name)
+print(data)
