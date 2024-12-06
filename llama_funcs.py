fa4828ec10479b4567b819bf5cc1f4cd8dc7bf7a
apxy36 <107977710+apxy36@users.noreply.github.com>
2024-12-06 23:04:37 +0800

    edit for llama70b

--- llama_funcs.py
+++ llama_funcs.py
@@ -240,7 +240,7 @@ def gen_prob(problem ,prompt, num=1, verify=False, include_eg = True):
         problist.append(logitz)
         tokenlist.append(tokens)
         responses.append(string_y)
-        selected_indices = [0, 8, 16, 24, 32] # [0, 16, 32, 48, 64, 80]
+        selected_indices = [0, 16, 32, 48, 64, 80] # [0, 8, 16, 24, 32]
         second_last_hs = hidden_states[-2]
         # print(len(second_last_hs), "SECOND LAST HS")
         selected_tensors = [second_last_hs[idx] for idx in selected_indices]
